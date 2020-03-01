278e4e200dd0b6317870aec39429fcaa3e9336ec
Adrian Czarnecki <adrian-czarnecki@live.com>
2020-03-01 18:50:09 -0500

    [DELETE-Feature] Adds DELETE post functionality

--- src/main/java/ca/utoronto/utm/mcs/post/Handle.java
+++ src/main/java/ca/utoronto/utm/mcs/post/Handle.java
@@ -12,11 +12,10 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import com.mongodb.client.FindIterable;
-import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.model.Filters;
 import com.sun.net.httpserver.HttpExchange;
 
-import ca.utoronto.utm.mcs.Memory;
 import ca.utoronto.utm.mcs.Utils;
 
 public class Handle {
@@ -42,40 +41,119 @@ public class Handle {
         	throw new JSONException("JSON does not contain _id and/or name");
         
         
-        
         // If id and name given, query only for id, if no ID found respond with 400 and DON'T check names
         if (idGiven && nameGiven) {
-        	if (ObjectId.isValid(id)) {
-        		Document send = new Document("_id", id);
-        		System.out.println(col.find(send).first()); //TODO null is being recieved
+        	
+        	// id is not valid type so no results will return
+        	if (!ObjectId.isValid(id)) {
+        		String response = "";
+	            r.sendResponseHeaders(400, response.length());
+	            OutputStream os = r.getResponseBody();
+	            os.write(response.getBytes());
+	            os.close();
+	            return;
+        	}
+        	
+        	FindIterable<Document> rtn = col.find(Filters.eq("_id", new ObjectId(id)));
+        	
+        	JSONArray output = new JSONArray();
+        	
+        	for(Document d:rtn)
+        		output.put(new JSONObject(d.toJson()));
+        	
+        	//id and name given, id found
+        	if (output.length() > 0) {
+	        	String response = ""+output.toString();
+	            r.sendResponseHeaders(200, response.length());
+	            OutputStream os = r.getResponseBody();
+	            os.write(response.getBytes());
+	            os.close();
+	            return;
+        	}
+        	//id and name given, id does not exist
+        	else {
+        		String response = "";
+	            r.sendResponseHeaders(400, response.length());
+	            OutputStream os = r.getResponseBody();
+	            os.write(response.getBytes());
+	            os.close();
+	            return;
+        	}
+        	
+        }
+        
+        
+        // If ONLY id given, respond with 404 if no posts found
+        else if (idGiven) {
+        	
+        	// id is not valid type so no results will return
+        	if (!ObjectId.isValid(id)) {
+        		String response = "";
+	            r.sendResponseHeaders(404, response.length());
+	            OutputStream os = r.getResponseBody();
+	            os.write(response.getBytes());
+	            os.close();
+	            return;
+        	}
+        	
+        	
+        	FindIterable<Document> rtn = col.find(Filters.eq("_id", new ObjectId(id)));
         		
-        		FindIterable<Document> rtn = col.find(send);
-        		JSONArray output = new JSONArray();
+        	JSONArray output = new JSONArray();
 
-        		for(Document d:rtn) {
-        			JSONObject tempJson = new JSONObject(d.toJson());
-        			output.put(tempJson);
-        			System.out.println(tempJson.toString());
-        		}
-        		
-        		
+        	for(Document d:rtn)
+        		output.put(new JSONObject(d.toJson()));
+        	
+        	
+        	//only id given, id found
+        	if (output.length() > 0) {
         		String response = ""+output.toString();
             	r.sendResponseHeaders(200, response.length());
             	OutputStream os = r.getResponseBody();
             	os.write(response.getBytes());
             	os.close();
+            	return;
         	}
+        	
         	else {
-        		System.out.println(-2);
+        		//only id given and id does not exist
+        		String response = "";
+            	r.sendResponseHeaders(404, response.length());
+            	OutputStream os = r.getResponseBody();
+            	os.write(response.getBytes());
+            	os.close();
+            	return;
         	}
-        }
-        // If ONLY id given, respond with 404 if no posts found
-        else if (idGiven) {
-        	//TODO
         }
         // If ONLY name given, respond with 404 if no posts found
         else {
-        	//TODO
+        	
+        	FindIterable<Document> rtn = col.find(Filters.regex("title", name));
+    		
+    		JSONArray output = new JSONArray();
+
+    		for(Document d:rtn) {
+    			output.put(new JSONObject(d.toJson()));
+    		}
+    		
+    		//Name matches at least 1 post
+    		if (output.length() > 0) {
+	    		String response = ""+output.toString();
+	        	r.sendResponseHeaders(200, response.length());
+	        	OutputStream os = r.getResponseBody();
+	        	os.write(response.getBytes());
+	        	os.close();
+	        	return;
+    		}
+    		//Name matches 0 posts
+    		else {
+    			String response = "";
+	        	r.sendResponseHeaders(404, response.length());
+	        	OutputStream os = r.getResponseBody();
+	        	os.write(response.getBytes());
+	        	os.close();
+	        	return;
+    		}
         }
         
         
@@ -93,24 +171,23 @@ public class Handle {
         String title, author, content;
         List<String> tags = new ArrayList<String>();
         
-        if (deserialized.has("title") && 
-        	deserialized.has("author") &&
-        	deserialized.has("content") &&
-        	deserialized.has("tags")) {
-            
-        	title = deserialized.getString("title");
-            author = deserialized.getString("author");
-            content = deserialized.getString("content");
-            
-            JSONArray arr = deserialized.getJSONArray("tags");
-            
-            for(int i = 0; i < arr.length(); i++){
-                tags.add(arr.getString(i));
-            }
-            
-        }
-        else
+        if (!deserialized.has("title") ||
+        	!deserialized.has("author") ||
+        	!deserialized.has("content") ||
+        	!deserialized.has("tags"))
         	throw new JSONException("JSON does not contain title, author, content, and/or tags");
+        
+        
+        title = deserialized.getString("title");
+        author = deserialized.getString("author");
+        content = deserialized.getString("content");
+        
+        JSONArray arr = deserialized.getJSONArray("tags");
+        
+        for(int i = 0; i < arr.length(); i++){
+            tags.add(arr.getString(i));
+        }
+        
         
         Document send = new Document("title", title)
         				.append("author", author)
@@ -129,12 +206,46 @@ public class Handle {
     	OutputStream os = r.getResponseBody();
     	os.write(response.getBytes());
     	os.close();
-        
+    	return;
         
 	}
 	
-	public static void handleDelete(HttpExchange r, MongoCollection<Document> col) throws JSONException, IOException {
+	public static void handleDelete(HttpExchange r, MongoCollection<Document> col) throws JSONException, IOException, Exception {
 		
+		String body = Utils.convert(r.getRequestBody());
+        JSONObject deserialized = new JSONObject(body);
+        
+        String id;
+		
+        if (!deserialized.has("_id"))
+        	throw new JSONException("JSON does not contain _id");
+        
+        id = deserialized.getString("_id");
+       	
+        if (ObjectId.isValid(id) && col.countDocuments(Filters.eq("_id", new ObjectId(id)))>0) {
+       		
+       		col.findOneAndDelete(Filters.eq("_id", new ObjectId(id)));
+       		
+       		String response = "";
+            r.sendResponseHeaders(200, response.length());
+           	OutputStream os = r.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+            return;
+       		
+        }
+        
+        else {
+        	
+        	String response = "";
+            r.sendResponseHeaders(404, response.length());
+           	OutputStream os = r.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+            return;
+        
+        	
+        }
 	}
 	
 }
